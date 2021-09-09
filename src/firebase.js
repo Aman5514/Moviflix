c6c4c23bcb4882ddc8e3f50bcd695f978560fd87
Aman5514 <86490131+Aman5514@users.noreply.github.com>
2021-09-09 17:39:33 +0530

    Update firebase.js
    Use own user firebase api key and configuration

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import firebase from 'firebase';
 import 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDbJjzeCuDoCKB6xw3AM58gDIyyx39-cn0",
-    authDomain: "disney-plus-movies.firebaseapp.com",
-    projectId: "disney-plus-movies",
-    storageBucket: "disney-plus-movies.appspot.com",
-    messagingSenderId: "548278133456",
-    appId: "1:548278133456:web:c3aa39b352427a984774d5",
-    measurementId: "G-YM7C97X9MB"
+     use your own firebase configuration
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
